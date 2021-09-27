89b133c22ace9cb2f5d16b6792b2c2a9987b9b83
tindra <advakhova@gmail.com>
2021-09-27 22:08:26 +0300

    Footer markup updated

--- build/assets/js/app.js
+++ build/assets/js/app.js
@@ -899,6 +899,21 @@ $(document).ready(function(){
         $toggle.addClass(activeClass);
     });
 });
+/* Video */
+let video = document.querySelector('video');
+let isPaused = false;
+let observer = new IntersectionObserver((entries, observer) => { 
+    entries.forEach(entry => {
+        if(entry.intersectionRatio!=1  && !video.paused){
+            video.pause();
+            isPaused = true;
+        }
+        else if(isPaused) {
+            video.play(); 
+            isPaused=false}
+        });
+    }, {threshold: 1});
+    observer.observe(video);
 /* Animate on Scroll */
 $(document).ready(function(){
     AOS.init({
---
+++ src/js/video.js
@@ -0,0 +1,15 @@
+/* Video */
+let video = document.querySelector('video');
+let isPaused = false;
+let observer = new IntersectionObserver((entries, observer) => { 
+    entries.forEach(entry => {
+        if(entry.intersectionRatio!=1  && !video.paused){
+            video.pause();
+            isPaused = true;
+        }
+        else if(isPaused) {
+            video.play(); 
+            isPaused=false}
+        });
+    }, {threshold: 1});
+    observer.observe(video);
