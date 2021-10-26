ca5ad91acf021e93f1e2d158cd76b31d9fd902b2
tindra <advakhova@gmail.com>
2021-10-26 16:59:27 +0300

    Fullpage test page added

--- src/js/tag.js
+++ src/js/tag.js
@@ -2,11 +2,20 @@
 $(document).ready(function(){
     let $tag = $('[data-element="tag"]');
     let $tagToggle = $('[data-element="tag-toggle"]');
+    let tagPopoverWidth = 365;
+
+    if ( $(window).width() < 375 ) {
+        tagPopoverWidth = 288;
+    }
 
     $tag.each(function() {
         let $tag = $(this);
         let tagWidth = $tag.outerWidth();
+        let tagHeight = $tag.height();
+        let $tagItem = $tag.closest('[data-element="tag-item"]');
 
+        $tagItem.width(tagWidth);
+        $tagItem.height(tagHeight);
         $tag.find('[data-element="tag-popover-bg"]').width(tagWidth);
     });
 
@@ -14,13 +23,25 @@ $(document).ready(function(){
         e.preventDefault();
 
         let $toggle = $(this);
-        let $item = $toggle.closest('[data-element="tag"]');
+        let $tag = $toggle.closest('[data-element="tag"]');
 
-        if ($item.hasClass(expandedClass)) {
-            $item.removeClass(expandedClass);
+        if ($tag.hasClass(expandedClass)) {
+            $tag.css('width', 'auto');
+            $tag.removeClass(expandedClass);
         } else {
+            $('.'+expandedClass+'[data-element="tag"]').css('width', 'auto');
             $('.'+expandedClass+'[data-element="tag"]').removeClass(expandedClass);
-            $item.addClass(expandedClass);
+
+            if ( $toggle.outerWidth() > tagPopoverWidth ) {
+                tagPopoverWidth = $toggle.outerWidth();
+            }
+
+            if ( ($(window).width() - $toggle.offset().left ) < tagPopoverWidth ) {
+                $tag.addClass('is-right');
+            }
+
+            $tag.addClass(expandedClass);
+            $tag.css('width', tagPopoverWidth);
         }
 
         return false;
