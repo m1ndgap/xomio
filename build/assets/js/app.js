44cd415324345291a420d1a80d9c416b1b5d994a
tindra <advakhova@gmail.com>
2021-10-29 17:26:08 +0300

    Vertical slider on IDX added

--- build/assets/js/app.js
+++ build/assets/js/app.js
@@ -1475,6 +1475,57 @@ $(document).ready(function(){
         $textSlider.slick('resize');
     });
 });
+/* Vertical slider */
+$(document).ready(function(){
+    let $verticalSliders = $('[data-slider="vertical-slider"]');
+
+    $verticalSliders.each(function(index, slider) {
+        let $slider = $(slider);
+        let $sliderParent = $slider.closest('[data-slider-parent]');
+        let $sliderNav = $sliderParent.find('[data-slider="slider-nav"]');
+        let maxHeight = -1;
+
+        $slider.on('init', function(event, slick, currentSlide) {
+            let $slider = $(slider);
+            let $slides = $slider.find('.slick-slide');
+
+            let tallestSlide = Math.max.apply(Math, $slides.map(function(index, el) {
+                return $(el)[0].scrollHeight;
+            }));
+
+            $slides.css('height', tallestSlide + 'px');
+        });
+
+        $slider.slick({
+            vertical: true,
+            dots: true,
+            arrows: false,
+            swipeToSlide: true,
+            autoplay: true,
+            autoplaySpeed: 5000,
+            asNavFor: $sliderNav
+        });
+
+        $sliderNav.slick({
+          arrows: false,
+          fade: true
+        }).on('afterChange', function(event, slick, currentSlide){
+            let $currentSlideEl = $(slick.$slides.get(currentSlide));
+
+            if ($currentSlideEl.find('video').length > 0) {
+                let video = $currentSlideEl.find('video')[0];
+                video.pause();
+                video.currentTime = 0;
+                video.play();
+            }
+        });
+
+        $(window).on('orientationchange resize', function() {
+            $slider.slick('resize');
+            $sliderNav.slick('resize');
+        });
+    });
+});
 /* Video */
 document.addEventListener('DOMContentLoaded', function() {
     let video = document.querySelector('video');
